259e17ef874091a378a3295f0828a10984b68b54
Eric Ribeiro <ericribeiro@outlook.com.br>
2022-02-11 11:36:36 -0800

    small change with "double" quotes in "commit" message

--- src/scripts/notify.sh
+++ src/scripts/notify.sh
@@ -35,6 +35,7 @@ BuildMessageBody() {
     fi
     # Insert the default channel. THIS IS TEMPORARY
     echo "$T2"
+    
     T2=$(echo "$T2" | jq ". + {\"channel\": \"$SLACK_DEFAULT_CHANNEL\"}")
     SLACK_MSG_BODY=$T2
 }
